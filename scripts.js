2cdcd3c3031c7ad305e10511663bc69ae6e73ff3
Santarola <55802048+Santarola@users.noreply.github.com>
2023-09-29 15:03:22 +0300

    Add hover feature
    Added an hover feature that makes square's background color red when mouse enters in its zone

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ const grid = document.querySelector('#grid');
 
 for (let i = 0; i < 256; i++) {
     let square = document.createElement('div');
+    square.classList.add("square");
     square.style.height = '0';
     square.style.width = '5%';
     square.style.paddingBottom = '5%';
@@ -11,3 +12,12 @@ for (let i = 0; i < 256; i++) {
     
     grid.appendChild(square);
 }
+
+square = document.querySelectorAll('.square');
+
+[...square].forEach(square => {
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'red';
+    });
+})
+
