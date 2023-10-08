b1f26b0674797e683ceaf9cb4f462311067c2d8d
Santarola <55802048+Santarola@users.noreply.github.com>
2023-10-08 18:09:12 +0300

    Add grid change and fill color on hover
    Added feature that let user change total number of squares, also added color fill on squares when mouse enters their zone

--- scripts.js
+++ scripts.js
@@ -1,23 +1,66 @@
 const grid = document.querySelector('#grid');
+const changeGrid = document.querySelector('.change');
 
-
-
-for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
-    square.classList.add("square");
-    square.style.height = '0';
-    square.style.width = '5%';
-    square.style.paddingBottom = '5%';
-    square.style.border = '1px solid black';
+function createGrid() {
     
-    grid.appendChild(square);
+    for (let i = 0; i < 256; i++) {
+        let square = document.createElement('div');
+        square.classList.add("square");
+        square.style.height = `15.63px`;
+        square.style.width = `15.63px`;
+        square.style.border = '1px solid black';
+        
+        grid.appendChild(square);
+
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'red';
+            })
+    }
+    
 }
 
-square = document.querySelectorAll('.square');
+function newGrid() {
+    let newSize;
+    newSize = prompt("Enter a size of grid within 100 (for example: 16x16 = 16)");
+    square = document.querySelectorAll('.square');
 
-[...square].forEach(square => {
-    square.addEventListener('mouseover', () => {
-        square.style.backgroundColor = 'red';
-    });
-})
+    square.forEach(square => {
+        square.remove();
+    })
+
+    for (i = 0; i < newSize * newSize; i++) {
+        square = document.createElement('div');
+        square.classList.add("square");
+        let squareSize = 250 / newSize;
+        square.style.height = `${squareSize}px`;
+        square.style.width = `${squareSize}px`;
+        square.style.border = '1px solid black';
+        
+        grid.appendChild(square);
+
+        
+       
+    }
+    
+    square = document.querySelectorAll('.square');
+
+    square.forEach(square => {
+        square.addEventListener('mouseenter', () => {
+            square.style.backgroundColor = 'red';
+            })
+    })
+    
+}
+
+createGrid();
+
+changeGrid.addEventListener('click', () => {
+    newGrid();
+ })
+
+
+
+
+
+
 
